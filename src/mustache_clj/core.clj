e817c8b8cdfefbbc189e2519f00a8354bdac2d3e
Niko <niko.nyrhila@gmail.com>
2016-10-25 22:45:21 +0300

    More refactoring, having explicit :type instead of using (first (:value token))

--- src/mustache_clj/core.clj
+++ src/mustache_clj/core.clj
@@ -2,68 +2,65 @@
   (:use clojure.test)
   (:require [clojure.string :as str]))
 
-(let [brackets     {\{ 1 \} 2}
-      bracket-vals {"{{" 1 "}}" -1 "{{{" 100 "}}}" -100}]
+(let [bracket-chars {\{ 1 \} 2}
+      bracket-vals  {"{{" 1 "}}" -1 "{{{" 10 "}}}" -10}
+      bracket-types {0 :const 1 :normal 10 :raw \# :path-start \/ :path-end}]
   (defn lexer [template]
+    ; Split a string to tokens, group by brackets, determine their types
     (let [template       (str/replace template #"\{\{&(.*?)\}\}" "{{{$1}}}")
-          tokens         (map (partial apply str) (partition-by #(brackets % 0) template))
+          tokens         (map (partial apply str) (partition-by #(bracket-chars % 0) template))
           bracket-counts (reductions + (map #(get bracket-vals % 0) tokens))
-          tokens         (filter #(not (contains? bracket-vals (:token %)))
-                           (map #(hash-map :token %1 :bracket %2) tokens bracket-counts))]
-      (let [path-tokens #{\# \/}
-            is-bracket? (fn [token type] (and (> (:bracket token) 0) (contains? path-tokens type)))
-            update-path (fn [token type path]
-                          (if (is-bracket? token type)
-                             (case type
-                               \# (conj path (apply str (rest (:token token))))
-                               \/ (pop  path))
-                             path))
+          tokens         (filter #(not (contains? bracket-vals (:value %)))
+                           (map #(hash-map :value %1 :type (bracket-types %2)) tokens bracket-counts))
+          tokens         (map #(if (= (:type %) :const) %
+                                (assoc % :type (bracket-types (first (:value %)) :reference))) tokens)]
+      ; Go through tokens again, keeping track of the path we are at. Originally these
+      ; were separate functions but it was easier to develop and test this way.
+      (let [push-path   (fn [path token] (conj path (apply str (rest (:value token)))))
+            update-path (fn [path token] (case (:type token)
+                                           :path-start (push-path path token)
+                                           :path-end   (pop path)
+                                           path))
             add-paths (fn [result path tokens]
                         (if-let [token (first tokens)]
-                          (let [new-path (update-path token (first (:token token)) path)]
+                          (let [new-path (update-path path token)]
                              (recur (conj result (assoc token :path new-path)) new-path (rest tokens)))
                           result))
-            tokens-with-paths (filter #(not (= \# (first (:token %)))) (add-paths [] [] tokens))]
-        tokens-with-paths))))
+            tokens-with-paths (add-paths [] [] tokens)]
+        (filter #(not= (:type %) :path-start)) tokens-with-paths))))
 
-(deftest test-lexer-token
+(deftest test-lexer
   (let [tokens (-> "Hello{{#names}}, {{name}} (kids{{#kids}} {{.}}{{/kids}}){{/names}}!" lexer)]
-    (is (= "." (-> tokens (nth 5) :token)))))
-
-(deftest test-lexer-paths
-  (let [paths (-> "Hello{{#names}}, {{name}} (kids{{#kids}} {{.}}{{/kids}}){{/names}}!" lexer)]
-    (is (= ["names" "kids"] (-> paths (nth 4) :path)))))
+    (is (= {:path ["names" "kids"], :value ".", :bracket 1} (nth tokens 5)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (let [path-at-lvl #(get (:path %2) %1)]
-  (defn tree-from-paths
-    ([tokens] (tree-from-paths tokens 0))
+  (defn parser
+    ([tokens] (parser tokens 0))
     ([tokens tree-lvl]
      (let [partitions      (partition-by (partial path-at-lvl tree-lvl) tokens)
            first-partition (first partitions)]
        (if (and
              (nil? (path-at-lvl tree-lvl first-partition))
-             (= 1 (count partitions)))
-         (filter #(not (= \/ (first (:token %))))    first-partition)
-         (map    #(tree-from-paths % (inc tree-lvl)) partitions))))))
+             (= 1  (count partitions)))
+         (filter #(not= (:type %) :path-end) first-partition)
+         (map    #(parser % (inc tree-lvl))  partitions))))))
 
-(deftest test-tree-from-paths
-  (let [nodes (-> "Hello{{#names}}, {{name}} (kids{{#kids}} {{.}}{{/kids}}){{/names}}!"
-                  lexer tokens-with-paths tree-from-paths)]
-    (is (=
-          {:path ["names" "kids"], :token " ", :bracket 0}
-          (-> nodes (nth 1) (nth 1) (nth 0))))))
+(deftest test-parser
+  (let [ast (-> "Hello{{#names}}, {{name}} (kids{{#kids}} {{.}}{{/kids}}){{/names}}!" lexer parser)]
+    (is (= {:path ["names" "kids"], :value " ", :bracket 0}
+           (-> ast (nth 1) (nth 1) (nth 0))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn replace-symbols [tokens data]
   (let [mapping {\& "&amp;" \\ "&quot;" \> "&lt;" \< "&gt;"}
         escape-html (fn [value] (apply str (map #(get mapping % %) value)))
         token-fun #(let [bracket (:bracket %)
-                         token   (:token   %)]
+                         token   (:value   %)]
                      (if (= 0 bracket) token
                        ((if (= 1 bracket) escape-html identity) (or (data (keyword token)) ""))))
         result (apply str (map token-fun tokens))]
-    {:tokens tokens :result result}))
+    {:values tokens :result result}))
 
 (defn render [template data]
   (replace-symbols (parse-template template) data))
